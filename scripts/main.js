d2093a071e3f254e663b455bf6a0b93a2c8320a9
Leônidas Santos <oi@ltco.com.br>
2021-07-08 22:44:42 -0300

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,1 @@
-feather.replace();{var ltcoPrintScreen=function(){html2canvas(document.body,{width:700,windowWidth:700}).then(function(a){var b=a.toDataURL('image/jpeg',.5);ltcoSendWhatsApp(b)})},ltcoSendWhatsApp=function(a){window.open('whatsapp://send?text='+encodeURIComponent(a),'_blank')},buttonPrintScreen=document.querySelector('.ltco_printscreen'),wrapperTest=document.querySelector('#root');/* function downloadURI(uri, name) {
-    var link = document.createElement("a");
-
-    link.download = name;
-    link.href = uri;
-    document.body.appendChild(link);
-    link.click();
-    //after creating link you should delete dynamic link
-    //clearDynamicLink(link);
-  } */buttonPrintScreen.addEventListener('click',ltcoPrintScreen)}
+feather.replace();{var ltcoClearDynamicLink=function(a){a&&document.body.removeChild(a)},ltcoDownloadImage=function(a,b){var c=document.createElement('a');c.download=b,c.href=a,document.body.appendChild(c),c.click(),ltcoClearDynamicLink(c)},ltcoPrintScreen=function(){var a=Date.now();html2canvas(document.body,{width:700,windowWidth:700}).then(function(b){var c=b.toDataURL('image/jpeg',1);ltcoDownloadImage(c,a+'_schedules_ctft.jpg')})},buttonPrintScreen=document.querySelector('.ltco_printscreen');buttonPrintScreen.addEventListener('click',ltcoPrintScreen)}
